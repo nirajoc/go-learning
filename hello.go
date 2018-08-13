7ce068ef0e88005fd47061075afa8b1cbe19a29c
nirajoc.niraj <nirajoc.niraj.singh@oneconvergence.com>
2018-08-13 14:36:57 +0530

    reverseing the string

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import ( 
+	"fmt"
+	"github.com/nirajoc/stringutil"
+
+)
 
 func main() {
+	fmt.Printf(stringutil.Reverse("!oG, olleH"))
 	fmt.Printf("Hello, world.\n")
 }
